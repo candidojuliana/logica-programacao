f1286818228bf2c30d3f0d2602bc649b28fe5562
Juliana Candido <dsantosjulianacandido@gmail.com>
2023-08-17 13:23:49 -0300

    exercicio 3

---
+++ exercicios/js/ex003.js
@@ -0,0 +1,13 @@
+// Pedir para usuário digitar dois números, depois mostrar na tela a subtração, multiplicação e divisão desses dois números
+
+var n1 = prompt("Digite um número");
+   m1 = parseInt(n1);
+
+var n2 = prompt("Digite outro número");
+   n2 = parseInt(n2);
+
+alert( "a subtração é: "+ (n1 - n2))
+
+alert( "a multiplicação é: "+ (n1 * n2))
+
+alert( "a divisão é: "+ (n1 / n2))
